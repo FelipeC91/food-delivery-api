9900c6132bda9bb1605c3024be35ea9c4166835d
Felipe Campos <feelipecampos91@gmail.com>
2026-08-11 19:08:56 -0300

    ajust over import.sql's inserts  and change uuid colum approach to mysql

--- src/main/java/com/mypersonalportifolio/food_delivery_api/domain/concept/DomainEntityUUID.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/domain/concept/DomainEntityUUID.java
@@ -1,13 +1,13 @@
 package com.mypersonalportifolio.food_delivery_api.domain.concept;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.util.UUID;
 
@@ -19,9 +19,14 @@ import java.util.UUID;
  @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public abstract class DomainEntityUUID {
 
-     @Id
-     @GeneratedValue
-     @UuidGenerator
-     @EqualsAndHashCode.Include
-    private UUID id;
+//     @Id
+//     @GeneratedValue
+//     @UuidGenerator
+//     @EqualsAndHashCode.Include
+@Id
+@GeneratedValue(strategy = GenerationType.UUID) // Spring Boot 3 / Jakarta EE 10 native feature
+@JdbcTypeCode(SqlTypes.VARCHAR)                 // Instructs Hibernate 6 to save as a String
+@Column(name = "id", length = 36, updatable = false, nullable = false)
+
+private UUID id;
 }
