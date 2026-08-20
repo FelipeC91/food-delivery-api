3eaf6f6e0babb7b5f7627787ba3873ae08bb1a99
Felipe Campos <feelipecampos91@gmail.com>
2026-08-20 15:47:45 -0300

    ajust imports

--- src/main/java/com/mypersonalportifolio/food_delivery_api/domain/model/FoodCategory.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/domain/model/FoodCategory.java
@@ -1,18 +1,12 @@
 package com.mypersonalportifolio.food_delivery_api.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mypersonalportifolio.food_delivery_api.domain.concept.DomainEntityUUID;
-import com.mypersonalportifolio.food_delivery_api.domain.model.Restaurant;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Table(name = "food_category")
 @Entity
