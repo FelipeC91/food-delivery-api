06796d736d0b077ed46a206c13b86fcdf557be88
Felipe Campos <feelipecampos91@gmail.com>
2026-08-21 19:05:49 -0300

    add equals and hash code

--- src/main/java/com/mypersonalportifolio/food_delivery_api/domain/concept/DomainEntityUUID.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/domain/concept/DomainEntityUUID.java
@@ -9,6 +9,7 @@ import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.type.SqlTypes;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @MappedSuperclass
@@ -19,14 +20,19 @@ import java.util.UUID;
  @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public abstract class DomainEntityUUID {
 
-//     @Id
-//     @GeneratedValue
-//     @UuidGenerator
-//     @EqualsAndHashCode.Include
+
 @Id
 @GeneratedValue(strategy = GenerationType.UUID) // Spring Boot 3 / Jakarta EE 10 native feature
 @JdbcTypeCode(SqlTypes.VARCHAR)                 // Instructs Hibernate 6 to save as a String
 @Column(name = "id", length = 36, updatable = false, nullable = false)
-
+@EqualsAndHashCode.Include
 private UUID id;
+
+
+ @Override
+ public boolean equals(Object o) {
+  if (o == null || getClass() != o.getClass()) return false;
+  DomainEntityUUID that = (DomainEntityUUID) o;
+  return Objects.equals(id, that.id);
+ }
 }
