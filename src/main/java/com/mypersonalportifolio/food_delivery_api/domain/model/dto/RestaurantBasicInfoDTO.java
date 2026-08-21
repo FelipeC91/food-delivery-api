3e4a2e50543b5b9182e08f169575af4b1aae0916
Felipe Campos <feelipecampos91@gmail.com>
2026-08-21 19:17:57 -0300

    add property and change query RestaurantbasicInfo approach

--- src/main/java/com/mypersonalportifolio/food_delivery_api/domain/model/dto/RestaurantBasicInfoDTO.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/domain/model/dto/RestaurantBasicInfoDTO.java
@@ -1,6 +1,19 @@
 package com.mypersonalportifolio.food_delivery_api.domain.model.dto;
 
-import java.math.BigDecimal;
+import com.mypersonalportifolio.food_delivery_api.domain.model.PaymentMethod;
+import com.mypersonalportifolio.food_delivery_api.domain.model.Restaurant;
 
-public record RestaurantBasicInfoDTO(String name, BigDecimal shippingCost, String foodCategoryName) {
+import java.math.BigDecimal;
+import java.util.List;
+
+public record RestaurantBasicInfoDTO(String name, BigDecimal shippingCost, String foodCategoryName, List<PaymentMethod> paymentMethods) {
+
+    public RestaurantBasicInfoDTO(Restaurant r) {
+        this(
+                r.getName(),
+                r.getShippingCost(),
+                r.getFoodCategory().getName(),
+                List.copyOf(r.getPaymentMethods())
+        );
+    }
 }
--- src/main/java/com/mypersonalportifolio/food_delivery_api/domain/repository/RestaurantRepository.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/domain/repository/RestaurantRepository.java
@@ -4,6 +4,7 @@ package com.mypersonalportifolio.food_delivery_api.domain.repository;
 import com.mypersonalportifolio.food_delivery_api.domain.model.Restaurant;
 import com.mypersonalportifolio.food_delivery_api.domain.model.dto.RestaurantBasicInfoDTO;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -20,9 +21,24 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, UUID>,
 
 
 
-    @Query("SELECT new com.mypersonalportifolio.food_delivery_api.domain.model.dto.RestaurantBasicInfoDTO(r.name, r.shippingCost, fc.name) " +
-           "FROM Restaurant r JOIN r.foodCategory fc")
-    List<RestaurantBasicInfoDTO> findAllReturningBasicInfo();
+//    @Query("""
+//             SELECT new com.mypersonalportifolio.food_delivery_api.domain.model.dto.RestaurantBasicInfoDTO(r.name, r.shippingCost, fc.name, pm)
+//                            FROM Restaurant AS r
+//                            JOIN r.foodCategory AS fc
+//                            LEFT JOIN r.paymentMethods pm
+//            """)
+//    List<RestaurantBasicInfoDTO> findAllReturningBasicInfo();
+
+    @EntityGraph(attributePaths = {"foodCategory", "paymentMethods"})
+    @Query("SELECT r FROM Restaurant AS r")
+    List<Restaurant> findAllWithGraph();
+
+
+    default List<RestaurantBasicInfoDTO> findAllReturningBasicInfo() {
+        return findAllWithGraph().stream()
+                .map(RestaurantBasicInfoDTO::new)
+                .toList();
+    }
 
     Optional<Restaurant> findFirstRestaurantByNameContaining(String name);
 
