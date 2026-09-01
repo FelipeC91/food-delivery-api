ed5b6c5cdea771d8fdab63e759e8b192f673ec8d
Felipe Campos <feelipecampos91@gmail.com>
2026-09-01 19:52:43 -0300

    change hash code approach

--- src/main/java/com/mypersonalportifolio/food_delivery_api/domain/concept/DomainEntityUUID.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/domain/concept/DomainEntityUUID.java
@@ -13,26 +13,27 @@ import java.util.Objects;
 import java.util.UUID;
 
 @MappedSuperclass
-
- @NoArgsConstructor
- @AllArgsConstructor
- @Getter
- @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public abstract class DomainEntityUUID {
 
 
-@Id
-@GeneratedValue(strategy = GenerationType.UUID) // Spring Boot 3 / Jakarta EE 10 native feature
-@JdbcTypeCode(SqlTypes.VARCHAR)                 // Instructs Hibernate 6 to save as a String
-@Column(name = "id", length = 36, updatable = false, nullable = false)
-@EqualsAndHashCode.Include
-private UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID) // Spring Boot 3 / Jakarta EE 10 native feature
+    @JdbcTypeCode(SqlTypes.VARCHAR)                 // Instructs Hibernate 6 to save as a String
+    @Column(name = "id", length = 36, updatable = false, nullable = false)
+    private UUID id;
 
+    @Override
+    public boolean equals(Object o) {
+         if (o == null || getClass() != o.getClass()) return false;
+         DomainEntityUUID that = (DomainEntityUUID) o;
+         return Objects.equals(id, that.id);
+    }
 
- @Override
- public boolean equals(Object o) {
-  if (o == null || getClass() != o.getClass()) return false;
-  DomainEntityUUID that = (DomainEntityUUID) o;
-  return Objects.equals(id, that.id);
+    @Override
+    public int hashCode() {
+         return Objects.hashCode(id);
  }
 }
