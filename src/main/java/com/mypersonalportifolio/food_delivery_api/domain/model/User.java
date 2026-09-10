11b95bc93d67d81a71387bd67d69ea6b1696e6a8
Felipe Campos <feelipecampos91@gmail.com>
2026-09-10 13:10:18 -0300

    add validations and create setters

--- src/main/java/com/mypersonalportifolio/food_delivery_api/domain/model/User.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/domain/model/User.java
@@ -5,27 +5,43 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Entity
 public class User extends DomainEntityUUID {
 
+    @Setter
     @Getter
+    @NotBlank
+    @Column(nullable = false)
     private String name;
 
+    @Setter
     @Getter
+    @NotBlank
+    @Email
+    @Column(nullable = false)
     private String email;
 
+    @Setter
     @Getter
+    @NotBlank
+    @Column(nullable = false)
     private String password;
 
     @Getter
     @CreationTimestamp
+    @Column(name = "create_at", nullable = false, updatable = false)
     private OffsetDateTime createdAt;
 
     @ManyToMany
@@ -33,7 +49,7 @@ public class User extends DomainEntityUUID {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns =  @JoinColumn(name = "user_group_id")
     )
-    private List<UserGroup> userGroups;
+    private List<UserGroup> userGroups = new ArrayList<>();
 
     public List<UserGroup> getUserGroups() {
         return Collections.unmodifiableList(this.userGroups);
