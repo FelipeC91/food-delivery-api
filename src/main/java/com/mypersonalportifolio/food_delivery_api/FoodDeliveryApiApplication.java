5e5af39af7138d3d12303021266835d2e4252c43
Felipe Campos <feelipecampos91@gmail.com>
2026-09-10 13:20:48 -0300

    enforce timezone config

--- src/main/java/com/mypersonalportifolio/food_delivery_api/FoodDeliveryApiApplication.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/FoodDeliveryApiApplication.java
@@ -1,10 +1,19 @@
 package com.mypersonalportifolio.food_delivery_api;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class FoodDeliveryApiApplication {
+
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryApiApplication.class, args);
