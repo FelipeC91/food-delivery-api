86ca23e530d10d5d59595a0b5f70e0990667f1e4
Felipe Campos <feelipecampos91@gmail.com>
2026-09-10 12:58:51 -0300

    set up validation constraints

--- src/main/java/com/mypersonalportifolio/food_delivery_api/domain/model/UserGroup.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/domain/model/UserGroup.java
@@ -5,14 +5,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Entity(name = "user_group")
+@Getter
 public class UserGroup extends DomainEntityUUID {
 
+    @NotBlank
+    @Column(nullable = false)
     private String name;
 
     @ManyToMany
@@ -24,5 +30,9 @@ public class UserGroup extends DomainEntityUUID {
 
     public List<Permission> getPermissions() {
         return Collections.unmodifiableList(this.permissions);
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
