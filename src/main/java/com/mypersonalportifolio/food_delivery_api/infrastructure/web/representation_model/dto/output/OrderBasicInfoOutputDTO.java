7bbd63f7b40abe9912f79f435e473426f058811a
Felipe Campos <feelipecampos91@gmail.com>
2026-09-22 00:26:03 -0300

    add order id

--- src/main/java/com/mypersonalportifolio/food_delivery_api/infrastructure/web/representation_model/dto/output/OrderBasicInfoOutputDTO.java
+++ src/main/java/com/mypersonalportifolio/food_delivery_api/infrastructure/web/representation_model/dto/output/OrderBasicInfoOutputDTO.java
@@ -2,14 +2,19 @@ package com.mypersonalportifolio.food_delivery_api.infrastructure.web.representa
 
 import com.mypersonalportifolio.food_delivery_api.domain.model.OrderStatus;
 import com.mypersonalportifolio.food_delivery_api.domain.model.PaymentMethod;
+import jdk.jfr.DataAmount;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.UUID;
 
 public record OrderBasicInfoOutputDTO(
+        UUID id,
         RestaurantBasicInfoOutputDTO restaurant,
         PaymentMethod paymentMethod,
         OffsetDateTime createdAt,
+        //@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
         OffsetDateTime confirmedAt,
         OffsetDateTime deliveredIn,
         OrderStatus orderStatus,
